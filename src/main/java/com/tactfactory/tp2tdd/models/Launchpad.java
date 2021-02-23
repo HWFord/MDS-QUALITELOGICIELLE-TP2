80644231fd0dcddfa696feb0f0bb65e3daeff0c3
Hannah <lilypickle@hotmail.fr>
2021-02-23 12:13:11 +0100

    empties temporarily Launchpad

--- src/main/java/com/tactfactory/tp2tdd/models/Launchpad.java
+++ src/main/java/com/tactfactory/tp2tdd/models/Launchpad.java
@@ -1,13 +1,4 @@
 package com.tactfactory.tp2tdd.models;
 
 public class Launchpad {
-  String name;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-}
--- src/test/java/com/tactfactory/tp2tdd/LaunchpadNameTest.java
+++ src/test/java/com/tactfactory/tp2tdd/LaunchpadNameTest.java
@@ -7,17 +7,6 @@ import com.tactfactory.tp2tdd.models.Launchpad;
 import com.tactfactory.tp2tdd.utils.GenericNameStartWithCapital;
 
 public class LaunchpadNameTest {
-  private Random rand = new Random();
 
-  /**
-   * test name starts with capital letter
-   */
-
-  @Test
-  public void testNameStartWithCapsOk() {
-    Launchpad launchpad = new Launchpad();
-    launchpad.setName(Character.toString((rand.nextInt(26) + 65) % 91));
-    GenericNameStartWithCapital.nameStartsWithCaps(launchpad.getName());
-  }
 
 }
