f3d94d53b138989b25ed9150da117dcb42c5d7b3
Hannah <lilypickle@hotmail.fr>
2021-02-23 12:09:12 +0100

    Fixes planet name test

--- src/test/java/com/tactfactory/tp2tdd/PlanetNameTest.java
+++ src/test/java/com/tactfactory/tp2tdd/PlanetNameTest.java
@@ -130,16 +130,54 @@ public class PlanetNameTest {
   }
 
   /**
-   * testNameLowerCase test function.
+   * testNameLowerCaseAaaA test function.
    *
    * @throws Exception to throw
    */
   @Test
-  public void testNameLowerCase() throws Exception {
+  public void testNameLowerCaseAaaA() throws Exception {
     Planet planet = new Planet();
     planet.setName("AaaaA");
     GenericNameTest.nameEndsWithLowerCase(planet.getName());
+    assertTrue(planet.getName().equals("Aaaaa"));
   }
+
+  /**
+   * testNameLowerCaseAAAA test function.
+   *
+   * @throws Exception to throw
+   */
+  @Test
+  public void testNameLowerCaseAAAA() throws Exception {
+    Planet planet = new Planet();
+    planet.setName("AAAAA");
+    assertTrue(planet.getName().equals("Aaaaa"));
+  }
+
+  /**
+   * testNameLowerCaseAAAA test function.
+   *
+   * @throws Exception to throw
+   */
+  @Test
+  public void testNameLowerCaseAa() throws Exception {
+    Planet planet = new Planet();
+    planet.setName("Aa");
+    GenericNameTest.nameEndsWithLowerCase(planet.getName());
+  }
+
+  /**
+   * testNameNameLowerCaseRandom test function.
+   *
+   * @throws Exception to throw
+   */
+  @Test
+  public void testNameNameLowerCaseRandom() throws Exception {
+	Planet planet = new Planet();
+    planet.setName(StringUtils.getRandomString());
+    GenericNameTest.nameEndsWithLowerCase(planet.getName());
+  }
+
 
 
 }
